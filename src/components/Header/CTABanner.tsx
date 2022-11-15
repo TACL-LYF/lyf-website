366370bb22ef7aa6b6f93ddc2e27d58629871593
Stephan Loh <sloh4137@gmail.com>
2022-11-15 01:24:37 -0800

    Prettier useBoop

---
+++ src/components/Header/CTABanner.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react"
+
+import { AppBar, Container, Link, Toolbar, Typography } from "@mui/material"
+import Grid from "@mui/material/Unstable_Grid2"
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt"
+import { animated } from "@react-spring/web"
+
+import useBoop from "@hooks/useBoop"
+
+type CTABannerProps = {
+  text: string
+  href: string
+}
+
+const AnimatedIcon = animated(ArrowRightAltIcon)
+
+export default function CTABanner({ text, href }: CTABannerProps) {
+  const [boopStyles, trigger] = useBoop({ x: 3, scale: 1.1 })
+
+  return (
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <Container maxWidth="sm">
+          <Link href={href} color="inherit" onMouseEnter={trigger}>
+            <Grid container justifyContent="center">
+              <Grid>{text} </Grid>
+              <Grid>
+                <AnimatedIcon style={boopStyles} />
+              </Grid>
+            </Grid>
+          </Link>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  )
+}
