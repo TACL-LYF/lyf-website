94fd4a324d7f7fd7926761e033882ae68eb501db
Stephan Loh <sloh4137@gmail.com>
2022-11-15 00:49:39 -0800

    Add CTABanner

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,6 +13,8 @@ import {
 import { Menu as MenuIcon } from "@mui/icons-material"
 import Logo from "@components/Logo"
 
+import CTABanner from "./CTABanner"
+
 const pages = [
   {
     to: "/about-us",
@@ -29,44 +31,55 @@ export default function Header() {
     setAnchorElNav(null)
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        {/* Menu for smaller screens. Hidden on larger screens */}
-        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-          <IconButton
-            size="large"
-            aria-label="Website nav menu"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            keepMounted
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
-          >
+    <>
+      <CTABanner
+        text={"Join our LIVE Virtual Info Session"}
+        href={"https://www.facebook.com/groups/291534277601982"}
+      />
+      <AppBar position="fixed" color="default">
+        <Toolbar>
+          {/* Menu for smaller screens. Hidden on larger screens */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="Website nav menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              keepMounted
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.text} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page.text}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Logo size={50} />
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <MenuItem key={page.text} onClick={handleCloseNavMenu}>
-                <Typography textAlign="center">{page.text}</Typography>
-              </MenuItem>
+              <Button
+                key={page.text}
+                onClick={handleCloseNavMenu}
+                sx={{ display: "block" }}
+                color="inherit"
+              >
+                {page.text}
+              </Button>
             ))}
-          </Menu>
-        </Box>
-
-        <Logo size={50} />
-
-        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-          {pages.map((page) => (
-            <Button key={page.text} onClick={handleCloseNavMenu} sx={{display: "block"}} color="inherit">
-              {page.text}
-            </Button>
-          ))}
-        </Box>
-      </Toolbar>
-    </AppBar>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
   )
 }
