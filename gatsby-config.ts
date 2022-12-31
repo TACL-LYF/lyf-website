44e50c9402532356836e7c07aad9037b33fa16f0
Stephan Loh <sloh@fb.com>
2022-12-31 03:40:21 -0500

    Add plugins necessary to display Sanity source and update homepage

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,7 +7,7 @@ const config: GatsbyConfig = {
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: false,
+  graphqlTypegen: true,
   plugins: [
     {
       resolve: "gatsby-source-sanity",
@@ -17,6 +17,8 @@ const config: GatsbyConfig = {
       },
     },
     "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
   ],
 }
 
