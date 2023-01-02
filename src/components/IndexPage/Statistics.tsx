e038e229a75792960e8193b929ca09e1d64efa46
Stephan Loh <sloh@fb.com>
2023-01-02 02:35:22 -0500

    Add section component and use it to create more sections for the home page

---
+++ src/components/IndexPage/Statistics.tsx
@@ -0,0 +1,62 @@
+import * as React from "react"
+import { Stack, Typography } from "@mui/material"
+import Grid from "@mui/material/Unstable_Grid2"
+import { graphql } from "gatsby"
+import { animated, useSpringValue, useInView, config } from "@react-spring/web"
+
+export const statFragment = graphql`
+  fragment Statistic on SanityStatistic {
+    number
+    decorator
+    caption
+  }
+`
+
+type StatProps = {
+  number: number,
+  decorator: string | null | undefined,
+  caption: string | null | undefined,
+}
+
+const AnimatedNumber = animated(Typography)
+
+const Stat = ({number, decorator, caption}: StatProps) => {
+  const value = useSpringValue(0, {
+    config: config.molasses
+  })
+  const [ref, inView] = useInView()
+  React.useEffect(() => {
+    value.start(inView ? number : 0)
+  }, [inView])
+
+  return (
+  <Stack alignItems="center">
+    <AnimatedNumber ref={ref} variant="h2" color="white">
+      {value.to((num => `${num.toFixed(0)}${decorator || ""}`))}
+    </AnimatedNumber>
+    <Typography variant="h6" textAlign="center" color="white">
+      {caption}
+    </Typography>
+  </Stack>
+)}
+
+
+type StatisticsProps = {
+  stats: readonly (Queries.StatisticFragment | null)[] | null
+}
+
+export default function Statistics({ stats }: StatisticsProps) {
+  if (!stats) {
+    return <></>
+  }
+
+  return (
+    <Grid container>
+      {stats.map((stat, index) => (
+        <Grid key={`${stat?.caption}-${index}`} xs={4}>
+          <Stat number={stat?.number || 0} decorator={stat?.decorator} caption={stat?.caption}/>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,39 @@
 import * as React from "react"
-import { Box, Container, Stack, Paper, Typography } from "@mui/material"
+import { Box, Container, Paper, Link, Stack, Typography } from "@mui/material"
 import Grid from "@mui/material/Unstable_Grid2"
 import { graphql, PageProps } from "gatsby"
 
-import { AnimatedButton } from "@components/Button"
+import { SanityButton } from "@components/Button"
 
-import { PhotoGrid } from "@components/IndexPage"
+import { PhotoGrid, Goals, Statistics } from "@components/IndexPage"
+import PortableText from "@components/PortableText"
+import { LinkWithIcon } from "@components/Link"
+import { Section } from "@components/Layout"
 
 export const query = graphql`
   query IndexPage {
     sanityHomePage {
       mainHeader
       subHeader
+      subHeaderButton {
+        ...SanityButton
+      }
       headerPhotos {
         ...SanityImageAsset
+      }
+      aboutHeader
+      _rawAboutBody
+      _rawCampDescription
+      goals
+      stats {
+        number
+        decorator
+        caption
+      }
+      ctaHeader
+      _rawCtaBody
+      ctaLink {
+        ...SanityButton
       }
     }
   }
@@ -21,20 +41,16 @@ export const query = graphql`
 
 export default function IndexPage({ data }: PageProps<Queries.IndexPageQuery>) {
   const { sanityHomePage } = data
+  if (!sanityHomePage) throw `No Sanity document for the home page was found.`
 
   return (
-    <Stack spacing={2} alignItems="stretch">
+    <Stack alignItems="stretch">
       {/* Hero Section */}
-      <Container
-        maxWidth="xl"
-        sx={{
-          padding: 4,
-        }}
-      >
+      <Section maxWidth={false}>
         <Grid
           container
           alignItems="center"
-          justifyContent="center"
+          justifyContent="space-between"
           flexWrap="wrap"
           gap={2}
         >
@@ -47,7 +63,7 @@ export default function IndexPage({ data }: PageProps<Queries.IndexPageQuery>) {
                 md: "left",
               }}
             >
-              {sanityHomePage?.mainHeader}
+              {sanityHomePage.mainHeader}
             </Typography>
           </Grid>
           {/* SubHeader */}
@@ -60,52 +76,89 @@ export default function IndexPage({ data }: PageProps<Queries.IndexPageQuery>) {
                   md: "left",
                 }}
               >
-                {sanityHomePage?.subHeader}
+                {sanityHomePage.subHeader}
               </Typography>
-              <AnimatedButton
+
+              <SanityButton
                 boopProps={{ scale: 1.1 }}
-                href="https://www.zeffy.com/en-US/ticketing/74df2944-47d2-4629-a3e3-0d8aa929fac3"
-                variant="contained"
+                content={sanityHomePage.subHeaderButton}
               >
                 Register
-              </AnimatedButton>
+              </SanityButton>
             </Stack>
           </Grid>
         </Grid>
-      </Container>
+      </Section>
 
       {/* Hero Images */}
-      <Container
+      <Section
         maxWidth={false}
         sx={{
-          paddingLeft: { xs: 1, md: 10 },
-          paddingRight: { xs: 1, md: 10 },
+          paddingTop: { xs: 0, md: 0 },
         }}
       >
-        <PhotoGrid photos={sanityHomePage?.headerPhotos} />
-      </Container>
+        <PhotoGrid photos={sanityHomePage.headerPhotos} />
+      </Section>
 
       {/* Info */}
-      <Paper
+      <Section
+        backgroundColor="secondary.light"
+        maxWidth="lg"
         sx={{
-          backgroundColor: "secondary.light",
-          padding: 10,
+          display: { xs: "none", md: "block" },
         }}
       >
-        <Container maxWidth="lg">
-          <Stack spacing={2}>
+        <Stack spacing={4}>
           <Typography variant="h3" textAlign="center">
-            We are a lifelong Taiwanese American community in the Bay Area -
-            built by the current generation of TAs for the next generation.
+            {sanityHomePage.aboutHeader}
           </Typography>
-          <Typography variant="body1" textAlign="center">
-            Our mission is to develop the Taiwanese American community into
-            whole person leaders through an understanding of heritage, self, and
-            the world.
+          <PortableText content={sanityHomePage._rawAboutBody} />
+        </Stack>
+      </Section>
+
+      {/* What is LYF Camp? */}
+      <Section backgroundColor="#F2F2F2">
+        <Grid container spacing={2} justifyContent="space-between">
+          <Grid xs={12} md={4}>
+            <Stack spacing={8}>
+              <Typography variant="h3" textAlign={{ xs: "center", md: "left" }}>
+                What is LYF Camp?
+              </Typography>
+              <PortableText
+                content={sanityHomePage._rawCampDescription}
+                spacing={2}
+              />
+              <LinkWithIcon
+                to="/"
+                text="Learn More"
+                color="inherit"
+                underline="hover"
+                textAlign="left"
+              />
+            </Stack>
+          </Grid>
+          <Grid xs={12} md={6}>
+            <Box
+              sx={{ width: 1, height: 1, backgroundColor: "primary.main" }}
+            ></Box>
+          </Grid>
+        </Grid>
+      </Section>
+
+      {/* Goals of LYF Camp */}
+      <Section>
+        <Stack spacing={5}>
+          <Typography variant="h3" textAlign={{ xs: "center", md: "left" }}>
+            Goals of LYF Camp
           </Typography>
-          </Stack>
-        </Container>
-      </Paper>
+          <Goals goals={sanityHomePage.goals} />
+        </Stack>
+      </Section>
+
+      {/* Statistics */}
+      <Section backgroundColor="tertiary.main">
+        <Statistics stats={sanityHomePage?.stats} />
+      </Section>
     </Stack>
   )
 }
