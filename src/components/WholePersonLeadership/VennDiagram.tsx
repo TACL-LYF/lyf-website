0e901897a63a083c7a7ac0a9bd06347f994b29b4
Stephan Loh <sloh@fb.com>
2023-01-12 00:45:07 -0800

    Add motion to the venn diagram

--- src/components/WholePersonLeadership/VennDiagram.tsx
+++ src/components/WholePersonLeadership/VennDiagram.tsx
@@ -1,34 +1,89 @@
 import * as React from "react"
-import { useSpring } from "@react-spring/web"
+import {
+  useSpring,
+  useSpringRef,
+  useChain,
+  animated as a,
+  config,
+  SpringRef,
+  SpringValue,
+} from "@react-spring/web"
 import { useTheme, styled } from "@mui/material"
 
-type TextProps = {
-  x: number,
-  y: number,
-  text: string,
-}
-
-const Text = styled("text")(({theme}) => ({
+const Text = styled("text")(({ theme }) => ({
   ...theme.typography.h5,
-  fill: "white"
+  fill: "white",
 }))
 
+type EllipseSpring = {
+  rx: SpringValue<number>
+  ry: SpringValue<number>
+  fillOpacity: SpringValue<number>
+}
+
+function useSpringAndRef(radius: number): [SpringRef, EllipseSpring] {
+  const springRef = useSpringRef()
+  const spring = useSpring({
+    ref: springRef,
+    from: { rx: 0, ry: 0, fillOpacity: 0 },
+    to: { rx: radius, ry: radius, fillOpacity: 0.8 },
+    config: config.wobbly,
+  })
+
+  return [springRef, spring]
+}
+
 export default function VennDiagram() {
-  const {shape, palette, typography } = useTheme()
-  const lineSpacing = typography.h5.lineHeight
-  console.log(lineSpacing)
+  const { shape, palette } = useTheme()
+
+  // We create three spring configs
+  const [firstSpringRef, firstSpring] = useSpringAndRef(160)
+  const [secondSpringRef, secondSpring] = useSpringAndRef(175)
+  const [thirdSpringRef, thirdSpring] = useSpringAndRef(206)
+
+  // Now chain those springs together at specific offset (0-1) times
+  useChain([firstSpringRef, secondSpringRef, thirdSpringRef], [0, 0.4, 0.8])
+
   return (
-    <svg height="640" width="640" style={{borderRadius: shape.borderRadius * 2, backgroundColor: "#F8F2F4"}}>
-      <ellipse cx="194" cy="352" rx="160" ry="160" fill={palette.secondary.main} fillOpacity={0.8}/>
-      <Text x={130} y={269}>
+    <svg
+      viewBox="0 0 640 640"
+      style={{
+        borderRadius: shape.borderRadius * 2,
+        backgroundColor: "#F8F2F4",
+      }}
+    >
+      <a.ellipse
+        cx="194"
+        cy="352"
+        fill={palette.secondary.main}
+        {...firstSpring}
+      ></a.ellipse>
+      <Text x={80} y={320}>
         Identity & Development
       </Text>
 
-      <ellipse cx="151" cy="568" rx="175" ry="175" fill={palette.primary.main} fillOpacity={0.8}/>
-      <Text x={77} y={505}>Acceptance & Belonging</Text>
+      <a.ellipse
+        cx="151"
+        cy="568"
+        fill={palette.primary.main}
+        {...secondSpring}
+      />
+      <Text x={60} y={515}>
+        Acceptance &
+      </Text>
+      <Text x={78} y={545}>
+        Belonging
+      </Text>
 
-      <ellipse cx="472" cy="536" rx="206" ry="206" fill={palette.tertiary.main} fillOpacity={0.8}/>
-      <Text x={384} y={481}>Leadership</Text>
+      <a.ellipse
+        cx="472"
+        cy="536"
+        fill={palette.tertiary.main}
+        {...thirdSpring}
+      />
+      <Text x={400} y={500}>
+        Leadership
+      </Text>
     </svg>
   )
 }
