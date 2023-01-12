fe3b16287a4df1cda9035f1ea64d2a5d9df8cc2c
Stephan Loh <sloh@fb.com>
2023-01-12 01:30:50 -0800

    Fix font weight

--- src/components/App/theme.ts
+++ src/components/App/theme.ts
@@ -33,9 +33,31 @@ const theme = createTheme({
   },
   typography: {
     fontFamily: '"Nunito", -apple-system, sans-serif',
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 700
+    },
+    h6: {
+      fontWeight: 700,
+    },
     subtitle1: bodyFontFamily,
     body1: bodyFontFamily,
-    body2: bodyFontFamily
+    body2: bodyFontFamily,
+    button: {
+      textTransform: "capitalize",
+      fontWeight: 700,
+    }
   }
 })
 
