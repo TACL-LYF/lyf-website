b986fcf9173b3e78dc646c6ec6f94d63df128a01
Kevin Lai <clai@ucsb.edu>
2023-03-26 00:35:28 -0700

    Up to volunteer impact section

--- src/pages/get-involved/join-our-team.tsx
+++ src/pages/get-involved/join-our-team.tsx
@@ -1,18 +1,49 @@
 import * as React from "react"
 import { PageProps, graphql } from "gatsby"
-import { Stack, Typography } from "@mui/material"
+import { Box, Stack, Typography } from "@mui/material"
+import Grid from "@mui/material/Unstable_Grid2"
 
 import { Section } from "@components/Layout"
 import getPageTitle from "@utils/getPageTitle"
+import { CardWithMedia } from "@components/Card"
+import { SanityButton } from "@components/Button"
+import SanityImage from "@components/Image/SanityImage"
 
 export const query = graphql`
   query JoinOurTeamPage {
     sanityJoinOurTeamPage {
       mainHeader
       subHeader
+      headerImage {
+        ...SanityImageAsset
+      }
+      headerButton {
+        ...SanityButton
+      }
+      volunteerImpact {
+        ...SanityCard
+      }
+      getInvolved {
+        ...SanityCard
+      }
+      interestForm
+      upNext {
+        ...SanityCard
+      }
     }
   }
 `
+
+const Placeholder = () => (
+  <Box
+    sx={{
+      width: 1,
+      height: 1,
+      backgroundColor: "primary.main",
+      borderRadius: 5,
+    }}
+  />
+)
 
 export const Head = getPageTitle("Join Our Team")
 
@@ -25,15 +56,54 @@ export default function JoinOurTeamPage({
 
   return (
     <>
-      <Section>
-        <Stack>
-          <Typography variant="h3">
-            {sanityJoinOurTeamPage?.mainHeader}
-          </Typography>
-          <Typography variant="h6">
-            {sanityJoinOurTeamPage?.subHeader}
-          </Typography>
-        </Stack>
+      {/* Header */}
+      <Section backgroundColor="tertiary.light">
+        <Grid container justifyContent="space-between" flexWrap="wrap">
+          {/* Header text + button */}
+          <Grid xs={12} lg={5}>
+            <Stack spacing={6} alignItems={{ xs: "center", md: "self-start" }} padding={{ xs: 2, lg: 12 }}>
+              <Typography variant="h3" color="black" textAlign={{ xs: "center", md: "left" }}>
+                {sanityJoinOurTeamPage?.mainHeader}
+              </Typography>
+              <Typography variant="h6" color="black" textAlign={{ xs: "center", md: "left" }}>
+                {sanityJoinOurTeamPage?.subHeader}
+              </Typography>
+              <SanityButton
+              content={sanityJoinOurTeamPage.headerButton}>
+              </SanityButton>
+            </Stack>
+          </Grid>
+          <Grid xs={12} md={6}>
+            <SanityImage imageAsset={sanityJoinOurTeamPage.headerImage}>
+            </SanityImage>
+          </Grid>
+        </Grid>
+      </Section>
+      {/* Why we volunteer section */}
+      <Section backgroundColor="secondary.light">
+        <Grid container alignItems="stretch" spacing={4}>
+          <Grid xs={12}>
+            <Stack spacing={6} alignItems="center">
+              <Typography variant="h3" textAlign="center">
+                Why do we volunteer?
+              </Typography>
+              <Typography variant="h6" textAlign="center">
+                TODO: get this content from graphql query
+              </Typography>
+            </Stack>
+          </Grid>
+          {sanityJoinOurTeamPage?.volunteerImpact?.map((reason) =>
+            <Grid key={reason?._key} xs={12} md={4}>
+            <CardWithMedia
+              header={reason?.title}
+              image={reason?.image}
+              content={reason?._rawDescription}
+              button={reason?.button}
+              shadowColor="secondary"
+            />
+            </Grid>
+          )}
+        </Grid>
       </Section>
     </>
   )
