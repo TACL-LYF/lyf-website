c9f2a2b0d6925e85659af9cba51c5b495c1ea6a6
Stephan Loh <sloh4137@gmail.com>
2023-05-08 01:01:46 -0700

    Remove unused components from leadership component files

--- src/components/Image/index.ts
+++ src/components/Image/index.ts
@@ -1,5 +1,4 @@
 export { default as SanityImage } from "./SanityImage"
 export { default as ImagesFullScreenViewer } from "./ImagesFullScreenViewer"
-export { default as PortraitImage } from "./PortraitImage"
 
 export type { sanityImageFragment } from "./SanityImage"
