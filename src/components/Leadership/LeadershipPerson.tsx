ad5e6e93686d368456a559541e2f0fe7521589ce
Stephan Loh <sloh4137@gmail.com>
2023-05-17 23:44:42 -0700

    Fix leadership images starting out as ellipse by adding a placeholder

--- src/components/Leadership/LeadershipPerson.tsx
+++ src/components/Leadership/LeadershipPerson.tsx
@@ -3,9 +3,9 @@ import React from "react"
 import { graphql } from "gatsby"
 import { Box, Stack, Typography } from "@mui/material"
 import SanityGatsbyImage from "gatsby-plugin-sanity-image"
+import useMeasure from "react-use-measure"
 
 import { SanityType } from "@utils/typeUtils"
-import { PortraitImage } from "@components/Image"
 
 export const sanityLeadershipPersonFragment = graphql`
   fragment SanityLeadershipPerson on SanityPerson {
@@ -27,18 +27,47 @@ export default function LeadershipPerson({
   backgroundColor,
 }: LeadershipPersonProps) {
   const propic = person?.propic
+  const [ref, bounds] = useMeasure()
+
   return (
-    <Stack spacing={2}>
+    <Stack spacing={2} ref={ref}>
       {propic ? (
-        <SanityGatsbyImage
-          {...propic}
-          width={200}
-          height={200}
-          style={{
-            borderRadius: "50%",
-            boxShadow: `8px 12px 0px ${backgroundColor}`
-          }}
-        />
+        <Box sx={{
+          position: "relative",
+          width: bounds.width,
+          height: bounds.width,
+          display: "inline-block",
+        }}>
+          <Box
+            sx={{
+              width: 1,
+              height: 1,
+              position: "absolute",
+              opacity: 1,
+              borderRadius: "50%",
+              boxShadow: `8px 12px 0px ${backgroundColor}`,
+              top: 0,
+              left: 0,
+              zIndex: 0,
+            }}
+          />
+          <SanityGatsbyImage
+            {...propic}
+            width={200}
+            height={200}
+            loading="lazy"
+            style={{
+              borderRadius: "50%",
+              position: "absolute",
+              boxShadow: `8px 12px 0px ${backgroundColor}`,
+              top: 0,
+              right: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: 100,
+            }}
+          />
+        </Box>
       ) : (
         <></>
       )}
