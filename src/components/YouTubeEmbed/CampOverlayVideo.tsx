96ffeb1be2b37d5a6908515901edca8cc492002b
Stephan Loh <sloh4137@gmail.com>
2023-05-17 23:06:31 -0700

    Make url nullable in camp overlay video

--- src/components/YouTubeEmbed/CampOverlayVideo.tsx
+++ src/components/YouTubeEmbed/CampOverlayVideo.tsx
@@ -3,14 +3,18 @@ import React from "react"
 import { Box } from "@mui/material"
 import useMeasure from "react-use-measure"
 
+import { SanityType } from "@utils/typeUtils"
 import YouTubeEmbed from "./YouTubeEmbed"
 import "./YouTubeBorderRadius.css"
 
 type CampOverlayVideoProps = {
-  url: string
+  url: SanityType<string>
 }
 
 export default function CampOverlayVideo({ url }: CampOverlayVideoProps) {
+  if (!url) {
+    return <></>
+  }
   const [ref, bounds] = useMeasure()
   return (
     <Box ref={ref} sx={{ width: 1, height: 1, borderRadius: 4 }}>
