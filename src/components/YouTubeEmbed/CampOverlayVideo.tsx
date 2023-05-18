601babee361bc1e06ee9f4f62eb3298d204f9eca
Stephan Loh <sloh@fb.com>
2023-05-17 19:01:22 -0700

    Add initial embed

---
+++ src/components/YouTubeEmbed/CampOverlayVideo.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import {Box} from "@mui/material"
+
+type CampOverlayVideoProps = {
+  url: string
+}
+
+export default function YouTubeEmbed({
+  url,
+}: CampOverlayVideoProps) {
+  const ref = React.useRef<HTMLDivElement>()
+  return (
+    <Box>
+    <YouTubeEmbed
+    />
+    </Box>
+  )
+}
