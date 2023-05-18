2a411f73d5e5f7c46c6d55f37af5c5a794739631
Stephan Loh <sloh4137@gmail.com>
2023-05-17 20:54:21 -0700

    Add xxl breakpoint

--- src/components/App/theme.ts
+++ src/components/App/theme.ts
@@ -69,6 +69,16 @@ const theme = createTheme({
       fontWeight: 700,
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+      xxl: 2000,
+    }
+  }
 })
 
 export default responsiveFontSizes(theme)
