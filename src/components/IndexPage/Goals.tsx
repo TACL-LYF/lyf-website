64cfed637cee8df2170b5eb848bdd2b3c8c78c68
Stephan Loh <sloh4137@gmail.com>
2023-05-20 23:44:13 -0700

    Make goals spacing better

--- src/components/IndexPage/Goals.tsx
+++ src/components/IndexPage/Goals.tsx
@@ -8,11 +8,11 @@ type GoalsProps = {
 }
 
 const icons = [
-  <ChatBubble color="primary" fontSize="large"/>,
+  <ChatBubble color="primary" fontSize="large" />,
   // @ts-ignore Color tertiary added in module augmentation but doesn't show up here.
-  <Twitter color="tertiary" fontSize="large"/>,
-  <Psychology color="secondary" fontSize="large"/>,
-  <Handshake color="primary" fontSize="large"/>,
+  <Twitter color="tertiary" fontSize="large" />,
+  <Psychology color="secondary" fontSize="large" />,
+  <Handshake color="primary" fontSize="large" />,
 ]
 
 export default function Goals({ goals }: GoalsProps) {
@@ -22,9 +22,9 @@ export default function Goals({ goals }: GoalsProps) {
 
   // Assume we're given 6 photos.
   return (
-    <Grid container justifyContent="space-between" alignItems="flex-start">
+    <Grid container justifyContent="space-between" alignItems="flex-start" spacing={2}>
       {goals.map((goal, index) => (
-        <Grid xs={12} md={2} key={goal}>
+        <Grid xs={12} sm={6} md={2} key={goal}>
           <Stack spacing={1}>
             {icons[index % 5]}
             <Typography variant="h6">{goal}</Typography>
