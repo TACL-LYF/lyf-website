c2ec94643b7736b5245043dc760db339696d0ed1
Stephan Loh <sloh4137@gmail.com>
2023-05-21 16:43:40 -0700

    Animate goals

--- src/components/IndexPage/Goals.tsx
+++ src/components/IndexPage/Goals.tsx
@@ -2,10 +2,13 @@ import * as React from "react"
 import { Stack, Typography } from "@mui/material"
 import { ChatBubble, Twitter, Psychology, Handshake } from "@mui/icons-material"
 import Grid from "@mui/material/Unstable_Grid2"
+import { animated, useInView, useTrail } from "@react-spring/web"
 
 type GoalsProps = {
   goals: readonly (string | null)[] | null
 }
+
+const AnimatedStack = animated(Stack)
 
 const icons = [
   <ChatBubble color="primary" fontSize="large" />,
@@ -20,15 +23,31 @@ export default function Goals({ goals }: GoalsProps) {
     return <></>
   }
 
+  const [ref, inView] = useInView({ once: false })
+  const [trails, __] = useTrail(
+    goals.length,
+    () => ({
+      translateX: inView ? 0 : -20,
+      opacity: inView ? 1 : 0,
+    }),
+    [inView]
+  )
+
   // Assume we're given 6 photos.
   return (
-    <Grid container justifyContent="space-between" alignItems="flex-start" spacing={2}>
+    <Grid
+      container
+      justifyContent="space-between"
+      alignItems="flex-start"
+      spacing={2}
+      ref={ref}
+    >
       {goals.map((goal, index) => (
         <Grid xs={12} sm={6} md={2} key={goal}>
-          <Stack spacing={1}>
+          <AnimatedStack spacing={1} style={trails[index]}>
             {icons[index % 5]}
             <Typography variant="h6">{goal}</Typography>
-          </Stack>
+          </AnimatedStack>
         </Grid>
       ))}
     </Grid>
