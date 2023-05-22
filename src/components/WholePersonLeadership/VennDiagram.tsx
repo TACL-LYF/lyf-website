cf320fb5489829397643c7a333097cc3241cb3b9
Stephan Loh <sloh4137@gmail.com>
2023-05-21 17:47:57 -0700

    Add in view start for the whole person leadership venn diagram model

--- src/components/WholePersonLeadership/VennDiagram.tsx
+++ src/components/WholePersonLeadership/VennDiagram.tsx
@@ -7,6 +7,7 @@ import {
   config,
   SpringRef,
   SpringValue,
+  useInView,
 } from "@react-spring/web"
 import { useTheme, styled } from "@mui/material"
 
@@ -35,14 +36,22 @@ function useSpringAndRef(radius: number): [SpringRef, EllipseSpring] {
 
 export default function VennDiagram() {
   const { shape, palette } = useTheme()
+  const [ref, inView] = useInView({ once: false })
 
   // We create three spring configs
   const [firstSpringRef, firstSpring] = useSpringAndRef(160)
   const [secondSpringRef, secondSpring] = useSpringAndRef(175)
   const [thirdSpringRef, thirdSpring] = useSpringAndRef(206)
 
-  // Now chain those springs together at specific offset (0-1) times
-  useChain([firstSpringRef, secondSpringRef, thirdSpringRef], [0, 0.4, 0.8])
+  // I would do this with useChain but they don't allow in view starting
+  // so I've mimicked it myself. Note that because we do "once" we won't
+  // allow setTimeout to conflict with itself.
+  React.useEffect(() => {
+    if (inView) {
+      firstSpringRef.start(), setTimeout(() => secondSpringRef.start(), 400)
+      setTimeout(() => thirdSpringRef.start(), 800)
+    }
+  }, [inView, firstSpringRef, secondSpringRef, thirdSpringRef])
 
   return (
     <svg
@@ -51,6 +60,7 @@ export default function VennDiagram() {
         borderRadius: shape.borderRadius * 2,
         backgroundColor: "#F8F2F4",
       }}
+      ref={ref}
     >
       <a.ellipse
         cx="194"
